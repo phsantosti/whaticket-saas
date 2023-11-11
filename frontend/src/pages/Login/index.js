f72a98b29107fc680ecf51e59ef7fe3bfe5a2d8f
Pedro Santos <phsantosti@gmail.com>
2023-11-10 23:54:52 -0300

    ajustes de cores e logo

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,40 +1,14 @@
 import React, { useState, useContext } from "react";
-import { Link as RouterLink } from "react-router-dom";
-
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import TextField from "@material-ui/core/TextField";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { Visibility, VisibilityOff } from '@material-ui/icons';
-
-import {
-	InputAdornment,
-	IconButton
-} from '@material-ui/core';
-
+import { InputAdornment, IconButton } from '@material-ui/core';
 import { i18n } from "../../translate/i18n";
-
 import { AuthContext } from "../../context/Auth/AuthContext";
 import logo from "../../assets/logologin.png";
-
-
-// const Copyright = () => {
-// 	return (
-// 		<Typography variant="body2" color="textSecondary" align="center">
-// 			{"Copyleft "}
-// 			<Link color="inherit" href="https://github.com/canove">
-// 				Canove
-// 			</Link>{" "}
-// 			{new Date().getFullYear()}
-// 			{"."}
-// 		</Typography>
-// 	);
-// };
 
 const useStyles = makeStyles(theme => ({
 	paper: {
@@ -80,9 +54,6 @@ const Login = () => {
 				<div>
 					<img style={{ margin: "0 auto", height: '100%', width: '100%',alignSelf: 'center' }} src={logo} alt="Whats" />
 				</div>
-				{/* <Typography component="h1" variant="h5">
-					{i18n.t("login.title")}
-				</Typography> */}
 				<form className={classes.form} noValidate onSubmit={handlSubmit}>
 					<TextField
 						variant="outlined"
@@ -131,21 +102,8 @@ const Login = () => {
 					>
 						{i18n.t("login.buttons.submit")}
 					</Button>
-					<Grid container>
-						<Grid item>
-							<Link
-								href="#"
-								variant="body2"
-								component={RouterLink}
-								to="/signup"
-							>
-								{i18n.t("login.buttons.register")}
-							</Link>
-						</Grid>
-					</Grid>
 				</form>
 			</div>
-			<Box mt={8}>{/* <Copyright /> */}</Box>
 		</Container>
 	);
 };
